a4cfefa8a033e7605911484dff99ce241c43109c
Ravi <raviranjanojha777@gmail.com>
2021-07-17 14:19:33 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,42 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const toPerson = {
-  fullName: 'Mr. Receiver',
-  address: '123 Fake st.\n Boston, MA 02118',
+const info = {
+  name: 'Mr. customer',
+  expirationDate: '05/25',
+  creditCardNumer: '5487 5695 2145 8623',
+  bankName: 'Deutch Bank',
 };
 
-const fromPerson = {
-  fullName: 'Mr. Sender',
-  address: '123 Fake st.\n San Francisco, CA 94101',
-};
-
-const AdressLabel = ({ person }) => {
+const CreditCard = ({ cardInfo }) => {
   return (
-    <div>
-      <div>{person.fullName}</div>
-      <div className='display-linebreak'>{person.address}</div>
-    </div>
-  );
-};
-
-const Envelope = ({ to, from }) => {
-  return (
-    <div className='envelope'>
-      <div className='stamp'>
-        <div className='stamp-text'>STAMP</div>
+    <div className='body'>
+      <div className='bank-name'>{cardInfo.bankName}</div>
+      <br />
+      <div className='card-number'>{cardInfo.creditCardNumer}</div>
+      <div className='card-number-f'>
+        {cardInfo.creditCardNumer.substring(0, 4)}
       </div>
-      <div className='sender'>
-        <AdressLabel person={from} />
+      <div className='exp'>
+        <span className='valid-thru'>VALID THRU </span>
+        {cardInfo.expirationDate}
       </div>
-      <div className='receiver'>
-        <AdressLabel person={to} />
-      </div>
+      <div className='name'>{cardInfo.name}</div>
     </div>
   );
 };
 
 ReactDOM.render(
-  <Envelope to={toPerson} from={fromPerson} />,
+  <CreditCard cardInfo={info} />,
   document.querySelector('#root')
 );
